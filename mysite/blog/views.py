eb1ab7170d7d5d5e3c81443523c5efa4e56b6178
stevekutz <stkutz@gmail.com>
2020-05-22 18:59:08 -0500

    added class-based view and link to return to main blog page from detail template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,11 +2,36 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    
+    paginator = Paginator(object_list, 3)   # provide 3 posts per page                          
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:    
+        # of page NOT an integer, provide first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if Page is out of range, provide last page    
+        posts = paginator.page(paginator.num_pages)
+    
+    
+    context = {'page': page, 'posts': posts, }
+    
+    return render(request, 'blog/post/list.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    # queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -20,17 +45,3 @@ def post_detail(request, year, month, day, post):
     context = {'post': post}
 
     return render(request, 'blog/post/detail.html', context)   
-
-def post_lisst(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)   # provide 3 posts per page                          
-    page = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:    
-        # of page NOT an integer, provide first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if Page is out of range, provide last page    
-        posts = paginator.page(paginator.num_pages)
